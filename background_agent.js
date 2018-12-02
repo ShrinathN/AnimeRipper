455a6f9dda98dcd7951431ad6271b1d0961c2a98
ShrinathN <shrinath.nimare@gmail.com>
2018-12-02 19:23:42 +0530

    Finally found a fix. Have to use chrome.tabs.sendMessage instead of chrome.runtime.sendMessage in background script

--- background_agent.js
+++ background_agent.js
@@ -7,67 +7,99 @@ function getRunningStatus() {
   });
 }
 
-document.body.onload = function() {
-  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.todo == "storeEpisodeList") //the main episode list is being sent in as data. Sent by episode_list.js
-    {
-      episodeList = request.data;
-      chrome.storage.sync.set({
-        episodeList: episodeList
-      }); //storing data
-    } else if (request.todo == "whatIsNextPage") //if a script is requesting the next page to go to
-    {
-      chrome.storage.sync.get("episodeList", function(data) {
-        episodeList = data.episodeList;
-        var nextPage = episodeList.pop();
-        if (nextPage == undefined) //meaning all have been gone over, process ends here
-        {
-          chrome.storage.sync.set({
-            isRunning: false
-          }); //storing data
-        } else { //meaning this was not the last element in the list
-          chrome.storage.sync.set({
-            episodeList: episodeList
-          }); //saving the new list
-          chrome.runtime.sendMessage({
-            todo: "goToPage",
-            data: nextPage
-          }); //sending message to script to goto the nextPage
+function sendNextPage() {
+  chrome.storage.sync.get("episodeList", function(data) {
+      episodeList = data.episodeList;
+      var nextPage = episodeList.pop();
+      var nextPage = episodeList.pop();
+      if (nextPage == undefined) //meaning all have been gone over, process ends here
+      {
+        chrome.storage.sync.set({
+          isRunning: false
+        }); //storing data
+      } else { //meaning this was not the last element in the list
+        chrome.storage.sync.set({
+          episodeList: episodeList
+        }); //saving the new list
+        chrome.tabs.query({
+            active: true,
+            currentWindow: true
+          }, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {
+              todo: "goToPage",
+              data: nextPage
+            }, function() {
+              alert("SnedMESSAGE");
+            });
+          });
         }
-      });
-    } else if (request.todo == "canIRun") {
-      getRunningStatus();
-      if (isRunning) {
-        chrome.runtime.sendMessage({
-          todo: "yesCanRun"
-        });
-      } else {
-        chrome.runtime.sendMessage({
-          todo: "noCannotRun"
-        });
-      }
-    } else if (request.todo == "addDownloadLink") {
-      var downloadLinkList = [];
-      var linkToAdd = request.data;
-      chrome.storage.sync.get("downloadLinkList", function(result) {
-        downloadLinkList = result.downloadLinkList;
-      });
-      downloadLinkList.push(linkToAdd);
-      chrome.storage.sync.set({
-        downloadLinkList: downloadLinkList
-      });
-      alert(downloadLinkList.pop());
-    }
   });
 }
-/*
-===LIST OF REQUESTS===
 
-storeEpisodeList
-whatIsNextPage -=-=-=-=- goToPage
-canIRun -=-=-= yesCanRun
-            -= noCannotRun
-addDownloadLink
+  document.body.onload = function() {
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+      if (request.todo == "storeEpisodeList") //the main episode list is being sent in as data. Sent by episode_list.js
+      {
+        episodeList = request.data;
+        chrome.storage.sync.set({
+          episodeList: episodeList
+        }); //storing data
+      }
+      // else if (request.todo == "whatIsNextPage") //if a script is requesting the next page to go to
+      // {
+      //   chrome.storage.sync.get("episodeList", function(data) {
+      //     episodeList = data.episodeList;
+      //     var nextPage = episodeList.pop();
+      //     if (nextPage == undefined) //meaning all have been gone over, process ends here
+      //     {
+      //       chrome.storage.sync.set({
+      //         isRunning: false
+      //       }); //storing data
+      //     } else { //meaning this was not the last element in the list
+      //       chrome.storage.sync.set({
+      //         episodeList: episodeList
+      //       }); //saving the new list
+      //       chrome.runtime.sendMessage({
+      //         todo: "goToPage",
+      //         data: nextPage
+      //       }); //sending message to script to goto the nextPage
+      //     }
+      //   });
+      // }
+      // else if (request.todo == "canIRun") {
+      //   getRunningStatus();
+      //   if (isRunning) {
+      //     chrome.runtime.sendMessage({
+      //       todo: "yesCanRun"
+      //     });
+      //   } else {
+      //     chrome.runtime.sendMessage({
+      //       todo: "noCannotRun"
+      //     });
+      //   }
+      // }
+      else if (request.todo == "addDownloadLink") {
+        var downloadLinkList = [];
+        var linkToAdd = request.data;
+        chrome.storage.sync.get("downloadLinkList", function(result) {
+          downloadLinkList = result.downloadLinkList;
+        });
+        downloadLinkList.push(linkToAdd);
+        chrome.storage.sync.set({
+          downloadLinkList: downloadLinkList
+        });
+        window.setTimeout(sendNextPage, 500);
+      }
+    });
+  }
+  /*
+  ===LIST OF REQUESTS===
 
-===LIST OF REQUESTS===
-*/
+  storeEpisodeList
+  whatIsNextPage -=-=-=-=- goToPage
+  canIRun -=-=-= yesCanRun
+              -= noCannotRun
+  addDownloadLink
+
+  ===LIST OF REQUESTS===
+  */
--- download_page.js
+++ download_page.js
@@ -1,19 +1,21 @@
 //will only run on download pages
 //first it must acertain if its okay to run
-//
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.todo == "goToPage") {
+    alert("WHOO");
     window.location.href = request.data;
   }
 });
-// do {
+
+
+window.setTimeout(ripLink, 1000);
+
+function ripLink() {
   var greenButtons = document.getElementsByClassName("bttn green");
   var downloadLink = greenButtons[0].href;
-// } while (greenButtons[0] != undefined);
-chrome.runtime.sendMessage({
-  todo: "addDownloadLink",
-  data: downloadLink
-});
-chrome.runtime.sendMessage({
-  todo: "whatIsNextPage"
-});
+  chrome.runtime.sendMessage({
+    todo: "addDownloadLink",
+    data: downloadLink
+  });
+}
